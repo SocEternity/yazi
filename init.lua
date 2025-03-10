71eb13ae3d3e71e79f5e382dcbd6f38de97436cd
SocEternity <2231609004@qq.com>
2025-03-10 21:43:54 +0800

    add plugin relative-motions

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
 -- plugins:全边界，为 Yazi 添加完整的边框，使其看起来更漂亮
 require("full-border"):setup({
 	-- Available values:
-	--                  ui.Border.PLAIN, 直角边框
-	--                  ui.Border.ROUNDED, 圆角边框
+	-- ui.Border.PLAIN, 直角边框
+	-- ui.Border.ROUNDED, 圆角边框
 	type = ui.Border.ROUNDED,
+})
+
+-- plugins: 绝对行号
+require("relative-motions"):setup({
+	show_numbers = "relative",
+	show_motion = true,
+	enter_mode = "first",
 })
