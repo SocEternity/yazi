87bed40dde76cc0c656013d6cbe70372ebe5fa0c
SocEternity <2231609004@qq.com>
2025-03-10 17:06:57 +0800

    add plugin full-border

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- plugins:全边界，为 Yazi 添加完整的边框，使其看起来更漂亮
+require("full-border"):setup({
+	-- Available values:
+	--                  ui.Border.PLAIN, 直角边框
+	--                  ui.Border.ROUNDED, 圆角边框
+	type = ui.Border.ROUNDED,
+})
