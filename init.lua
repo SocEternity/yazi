a6e1703d2feb6f096473bf6e00e97d3022c85863
SocEternity <2231609004@qq.com>
2025-03-21 21:03:14 +0800

    add plugins searchjump

--- init.lua
+++ init.lua
@@ -12,3 +12,19 @@ require("relative-motions"):setup({
 	show_motion = true,
 	enter_mode = "first",
 })
+
+-- plugins:搜索跳转
+require("searchjump"):setup({
+	unmatch_fg = "#b2a496",
+	match_str_fg = "#000000",
+	match_str_bg = "#73AC3A",
+	first_match_str_fg = "#000000",
+	first_match_str_bg = "#73AC3A",
+	lable_fg = "#EADFC8",
+	lable_bg = "#BA603D",
+	only_current = false,
+	show_search_in_statusbar = false,
+	auto_exit_when_unmatch = false,
+	enable_capital_lable = true,
+	search_patterns = { "hell[dk]d", "%d+.1080p", "第%d+集", "第%d+话", "%.E%d+", "S%d+E%d+" },
+})
